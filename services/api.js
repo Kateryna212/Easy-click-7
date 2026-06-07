106abfe51747ec55d404215136a53dd07ae75470
Kateryna212 <koretcka.kate@e-u.edu.ua>
2026-06-07 23:13:50 +0300

    Add fetchProducts function to retrieve product list

---
+++ services/api.js
@@ -0,0 +1,7 @@
+export function fetchProducts() {
+    return [
+        { id: 1, name: "Ноутбук", price: 35000, image: "laptop.jpg" },
+        { id: 2, name: "Смартфон", price: 15000, image: "phone.jpg" },
+        { id: 3, name: "Навушники", price: 2500, image: "headphones.jpg" }
+    ];
+}
