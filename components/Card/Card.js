2975bc92121ca7c43bbdf6480a74ac6803e05ad2
Kateryna212 <koretcka.kate@e-u.edu.ua>
2026-06-08 01:02:59 +0300

    Update Card component with new button and alert message

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,18 +1,17 @@
-// Чиста функція: приймає дані (props) -> повертає розмітку
 export function Card({ title, price, imageUrl }) {
     const card = document.createElement('div');
     card.className = 'product-card';
 
     card.innerHTML = `
-        <img src="${imageUrl}" alt="${title}" class="card-img" />
+        <div class="card-img">${imageUrl}</div>
         <h3 class="card-title">${title}</h3>
         <p class="card-price">${price} грн</p>
-        <button class="card-btn">Купити</button>
+        <button class="card-btn">Купити в один клік</button>
     `;
 
-    // Приклад ізольованої логіки всередині UI-компонента
+    // Додаємо інтерактивність (швидкий клік)
     card.querySelector('.card-btn').addEventListener('click', () => {
-        alert(`Товар "${title}" додано до кошика!`);
+        alert(`Дякуємо! Товар "${title}" успішно оформлено через Easy-click!`);
     });
 
     return card;
